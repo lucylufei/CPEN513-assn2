7f280925412a57bdf9b5619218fd3f42c2db5f15
lucylufei <lucylufei@users.noreply.github.com>
2021-02-21 21:24:32 -0800

    added comments

--- annealing.py
+++ annealing.py
@@ -12,31 +12,50 @@ np.set_printoptions(threshold=np.inf, linewidth=np.inf)
 
 
 class SimAnneal:
+    '''
+    Implementation of Simulated Annealing
+    '''
     
     def __init__(self, canvas, ax):
+        '''
+        Initialize class with permanent variables
+        '''
         self.c = canvas
         self.ax = ax
         
+        
     def setup(self, configs, nets):
+        '''
+        Setup the simulation with the current circuit
+        '''
+        
+        # Circuit parameters
         self.configs = configs
         self.nets = nets
         
+        # Initialize placement map (NaN for empty cells)
         self.placement = np.zeros((configs["cols"], configs["rows"]))
         self.placement[:] = np.NaN
         random.seed()
         
+        # Initialize simulation variables
         self.cells = {}
         self.cost = {}
         self.new_cost = {}
         self.current_cost = 0
         self.delta_cost = 0
         self.temperature = start_temperature
+        self.iteration = 0
+        self.exit_tracker = 0
+        self.initalized = False
         
+        # Set number of moves per temperature (either based on circuit size or a fixed value)
         if dynamic_n_moves:
             self.n_moves = round(k_n_moves * math.pow(configs["cells"], float(4)/3))
         else:
             self.n_moves = n_moves
         
+        # Label temperature
         self.c.create_text(
             grid["right"] - 100,
             20,
@@ -46,82 +65,125 @@ class SimAnneal:
             tag="temp"
         )
         
-        self.iteration = 0
-        self.exit_tracker = 0
-        self.initalized = False
-        
     
     def animate(self, frame=None):
+        '''
+        Update matplotlib graph
+        '''
+        
+        # Update graph if the initial cost has already been calculated
         if self.initalized:
             self.ax.clear()
             self.ax.plot(self.x, self.y)
         
+        
     def random_placement(self):
+        '''
+        Initialize circuit with a random placement
+        '''
         # Reset all placement
         self.placement[:] = np.NaN
         
         # Make a list of possible coordinates
         possible_placements = [(x, y) for x in range(0, self.configs["cols"]) for y in range(0, self.configs["rows"])]
         
+        # Place every cell
         for i in range(self.configs["cells"]):
             (x, y) = random.choice(possible_placements)
             possible_placements.remove((x, y))
+            # Track which cell is in which coordinate
             self.placement[x, y] = i
+            # Track which coordinate each cell is at
             self.cells[i] = (x, y)
             
+        # Double check that all cells have been accounted for
         assert len(self.cells) == self.configs["cells"]
         
         debug_print("Current Placement:")
         debug_print(self.placement)
         
+        # Update the GUI
         self.draw_connections()
         self.update_labels()
+        
+        # Calculate the initial cost
         self.calculate_cost()
         
+        # Mark circuit as initialized
         self.initalized = True
+        
+        # Track graph x and y
         self.x = [0]
         self.y = [self.current_cost]
+        
+        # Initialize plot
         self.ax.plot(self.x, self.y)
         self.ax.set_ylabel("Half Perimeter Cost")
         self.ax.set_xlabel("Iteration")
 
                 
     def update_labels(self):
+        '''
+        Update cell labels on GUI
+        '''
+        
+        # Delete all previous labels
         self.c.delete("cell")
+        # Add new labels
         for cell in self.cells:
             add_text(self.cells[cell][0], self.cells[cell][1], self.c, grid, cell, tag="cell")
         
         
     def draw_connections(self):
+        '''
+        Update connections on GUI
+        '''
+        
+        # Delete all previous wires
         self.c.delete("wires")
+        # Draw new wires
         for i, net in enumerate(self.nets):
+            # Draw from first cell to the rest
+            orig = self.cells[net[0]]
             for cell in net[1:]:
-                orig = self.cells[net[0]]
                 dest = self.cells[cell]
+                # Draw a curve
                 if line_curve:
                     draw_line(orig, dest, self.c, grid, colour=wire_colour_palette[i % len(wire_colour_palette)], tag="wires", extra_point=i)
+                # Draw a line
                 else:
                     draw_line(orig, dest, self.c, grid, colour=wire_colour_palette[i % len(wire_colour_palette)], tag="wires")
         
             
     def calculate_cost(self):
+        '''
+        Calculate the half perimeter cost
+        '''
         
+        # Track the cost for each net
         for i, net in enumerate(self.nets):
             self.cost[i] = calculate_half_perimeter(net, self.cells)
             
+        # Sum the cost and update globally
         self.update_cost()
+        
             
     def calculate_delta_cost(self, cell1, cell2, temp_placement, additional_cells=None):
+        '''
+        Calculate incremental half perimenter cost
+        '''
         delta_cost = 0
         
+        # Check each net to see if the cost has changed
         for i, net in enumerate(self.nets):            
             cost_updated = False
+            # If the cells being swapped belong to this net, update the cost
             if cell1 in net or cell2 in net:
                 self.new_cost[i] = calculate_half_perimeter(net, temp_placement)
                 delta_cost += self.new_cost[i] - self.cost[i]
                 cost_updated = True
                 
-            # If there are more than 2 cells that have been updated
+            # If there are more than 2 cells that have been updated, update the cost for any nets with those cells
             elif additional_cells:
                 for cell in additional_cells:
                     if cell["cell"] in net:
@@ -129,25 +191,44 @@ class SimAnneal:
                         self.new_cost[i] = calculate_half_perimeter(net, temp_placement)
                         delta_cost += self.new_cost[i] - self.cost[i]
                         cost_updated = True
-                        
+
+            # If the net has not changed, set new cost equal to old cost
             if not cost_updated:
                 self.new_cost[i] = self.cost[i]
             
         debug_print("Delta Cost: {}".format(delta_cost))
+        
+        # Return just the change in cost
         return delta_cost
         
+        
     def update_cost(self):
+        '''
+        Update cost globally
+        '''
+        
+        # Sum up total cost
         total_cost = sum(self.cost[i] for i in range(self.configs["nets"]))
         debug_print("Total Cost: {}".format(total_cost))
+        
+        # Update global cost
         self.current_cost = total_cost
         
+        
     def no_exit(self):
+        '''
+        Exit condition
+        '''
+        
+        # Exit once the temperature is below a threshold
         if exit_criteria == "temp":
             return self.temperature > exit_temperature
             
+        # Exit if there is no improvement and the temperature is already at 0
         elif exit_criteria == "no_improvement":
             return self.delta_cost < 0 or self.temperature != 0
             
+        # Exit if there have been multiple iterations of no improvement and the temperature is already at 0
         elif exit_criteria == "multiple_no_improvement":
             if self.delta_cost > 0 and self.temperature == 0:
                 self.exit_tracker += 1
@@ -155,50 +236,74 @@ class SimAnneal:
                 self.exit_tracker = 0
             return self.exit_tracker < exit_iterations
             
-            
+        # Otherwise, the exit condition is missing or incorrect
         else:
             raise Exception
             
+            
     def update_swap(self, cell1, cell2, cell1_xy, cell2_xy, additional_cells=None):
+        '''
+        Commit the current swap
+        '''
+        
+        # Move cell1 to the location of cell2
         self.placement[cell2_xy] = cell1
         if not np.isnan(cell1):
             self.cells[cell1] = cell2_xy
             
+        # For settings with multiple swaps
         if additional_cells:
             prev_cell = cell2
+            
+            # Iteratively update each cell (moving cell2 to cell3, cell3 to cell4, ...)
             for additional_cell in additional_cells:
                 self.placement[additional_cell["cell_xy"]] = prev_cell
                 if not np.isnan(prev_cell):
                     self.cells[prev_cell] = additional_cell["cell_xy"]
                 prev_cell = additional_cell["cell"]
-                    
+        
+        # Otherwise, just perform a simple swap
         else:
+            # Move cell2 to the location of cell1
             self.placement[cell1_xy] = cell2
             if not np.isnan(cell2):
                 self.cells[cell2] = cell1_xy
         
+        # Update the official cost
         self.cost = copy.deepcopy(self.new_cost)
         debug_print("New Placement:")
         debug_print(self.placement)
         
+        # Sum up the total cost and update the canvas
         self.update_cost()
         self.c.update()
         
+        # Update GUI elements
         if update_gui:
             self.draw_connections()
             self.update_labels()
             time.sleep(display_delay)
         
-    def anneal(self):
         
+    def anneal(self):
+        '''
+        Swaps at one temperature
+        '''
+        
+        # Track number of accepted swaps
         accepted_swap = 0
+        
+        # Run for n moves
         for i in range(self.n_moves):
             self.iteration += 1
+            
+            # Create a temperary placement map
             temp_placement = copy.deepcopy(self.cells)
             
-            # Choose random cells to swap
+            # Make a list of cells
             cells = list(range(0, self.configs["cells"]))
             
+            # Choose random cells to swap
             cell1 = random.choice(cells)
             cell1_xy = self.cells[cell1]
             cells.remove(cell1)
@@ -208,6 +313,8 @@ class SimAnneal:
             
             # Choose another cell within the range
             if range_window:
+                
+                # Calculate the window
                 window_x = round(window_size*self.configs["cols"])
                 window_y = round(window_size*self.configs["rows"])
                 x_min = 0 if cell1_xy[0] - window_x < 0 else cell1_xy[0] - window_x
@@ -217,6 +324,7 @@ class SimAnneal:
                 
                 debug_print("Cell1: ({x}, {y}) -> range ({xmin}-{xmax}, {ymin}-{ymax})".format(x=cell1_xy[0], y=cell1_xy[1], xmin=x_min, xmax=x_max, ymin=y_min, ymax=y_max))
                 
+                # Randomly pick second location
                 cell2_xy = (random.randint(x_min, x_max), random.randint(y_min, y_max))
                 
                 # Make sure the swap isn't with itself
@@ -224,13 +332,17 @@ class SimAnneal:
                     cell2_xy = (random.randint(x_min, x_max), random.randint(y_min, y_max))
                 
                 cell2 = self.placement[cell2_xy]
+                
             # Choose any cell
             else:
                 cell2 = random.choice(cells)
                 
+            # If using range window, handle case of second choice being an empty cell
             if not range_window:
                 if cell2 == self.configs["cells"]:
                     cell2 = np.NaN
+                    
+                    # Choose any NaN (empty) cell location 
                     while True:
                         x = random.randint(0, self.configs["cols"]-1)
                         y = random.randint(0, self.configs["rows"]-1)
@@ -242,9 +354,11 @@ class SimAnneal:
                 else:
                     cell2_xy = self.cells[cell2]
                 
+            # If using "ripple" configuration (only valid if cell2 is not an empty cell)
             if ripple and not np.isnan(cell2):
                 additional_cells = []
                 
+                # Choose another cell in a new window
                 window_x = round(window_size*self.configs["cols"])
                 window_y = round(window_size*self.configs["rows"])
                 x_min = 0 if cell2_xy[0] - window_x < 0 else cell2_xy[0] - window_x
@@ -252,6 +366,7 @@ class SimAnneal:
                 y_min = 0 if cell2_xy[1] - window_y < 0 else cell2_xy[1] - window_y
                 y_max = self.configs["rows"]-1 if cell2_xy[1] + window_y >= self.configs["rows"] else cell2_xy[1] + window_y
                 
+                # Randomly pick a location in the new window
                 cell3_xy = (random.randint(x_min, x_max), random.randint(y_min, y_max))
                 
                 # Make sure the swap isn't with itself
@@ -259,6 +374,8 @@ class SimAnneal:
                     cell3_xy = (random.randint(x_min, x_max), random.randint(y_min, y_max))
                     
                 cell3 = self.placement[cell3_xy]
+                
+                # Add to list of additional cells involved in the swap
                 additional_cells.append({"cell": cell3, "cell_xy": cell3_xy})
                     
                 # Keep finding next cell until an empty spot is chosen
@@ -278,18 +395,28 @@ class SimAnneal:
                         cell3_xy = (random.randint(x_min, x_max), random.randint(y_min, y_max))
                         cell3 = self.placement[cell3_xy]
                         counter += 1
+                        
+                        # If it has taken more than 10 iterations, there probably isn't a possible cell
                         if counter > 10:
+                            # Use cell1 location as backup
                             cell3 = cell1
                             cell3_xy = cell1_xy
                             break
                         
+                    # Add to list of additional cells
                     additional_cells.append({"cell": cell3, "cell_xy": cell3_xy})
+                    
+                    # Prevent infinite loop
                     if counter > 10:
                         break
                     
                 debug_print(additional_cells)
                 
+                
+            # If using "shuffle" configuration (only valid if cell2 is not an empty cell)
             elif shuffle and not np.isnan(cell2):
+                
+                # Check 4 closest placements
                 possible_placements = [
                     (cell2_xy[0] + 1, cell2_xy[1]),
                     (cell2_xy[0] - 1, cell2_xy[1]),
@@ -297,18 +424,23 @@ class SimAnneal:
                     (cell2_xy[0], cell2_xy[1] - 1),
                 ]
                 
+                # Choose one of the placements
                 cell3_xy = random.choice(possible_placements)
                 while True:
                     try:
                         cell3 = self.placement[cell3_xy]
+                        
+                        # Check if cell is empty
                         if np.isnan(cell3):
                             break
                         else:
                             possible_placements.remove(cell3_xy)
+                            
                     except IndexError:
                         # Location is out of range
                         possible_placements.remove(cell3_xy)
                         
+                    # If no more options, resort to swapping back with cell1
                     if len(possible_placements) == 0:
                         cell3_xy = cell1_xy
                         break
@@ -319,6 +451,8 @@ class SimAnneal:
                 
             debug_print("Swapping cell {c1} ({x1}, {y1}) with cell {c2} ({x2}, {y2})".format(c1=cell1, c2=cell2, x1=cell1_xy[0], y1=cell1_xy[1], x2=cell2_xy[0], y2=cell2_xy[1]))
             
+            
+            # Update placement
             if shuffle and not np.isnan(cell2):
                 debug_print("Swapping cell {c1} ({x1}, {y1}) with cell {c2} ({x2}, {y2})".format(c1=cell2, c2=cell3, x1=cell2_xy[0], y1=cell2_xy[1], x2=additional_cells[0]["cell_xy"][0], y2=additional_cells[0]["cell_xy"][1]))
                 temp_placement[cell2] = cell3_xy
@@ -343,6 +477,7 @@ class SimAnneal:
                 # Check updated cost
                 self.delta_cost = self.calculate_delta_cost(cell1, cell2, temp_placement, additional_cells)
                 
+            # Update placement
             else:
                 additional_cells = None
                 if not np.isnan(cell2):
@@ -353,10 +488,13 @@ class SimAnneal:
                 # Check updated cost
                 self.delta_cost = self.calculate_delta_cost(cell1, cell2, temp_placement)
             
+            
+            # If cost has decreased, swap is automatically accepted
             if self.delta_cost < 0:
                 self.update_swap(cell1, cell2, cell1_xy, cell2_xy, additional_cells)
                 accepted_swap += 1
                 
+            # Otherwise accept with probability
             elif self.temperature != 0:
                 # Check if swap should happen
                 r = random.uniform(0, 1)
@@ -370,21 +508,26 @@ class SimAnneal:
                 else:
                     debug_print("Swap rejected")
                     
+            # If temperature is 0, the swap must decrease cost to be accepted
             else:
                 debug_print("Swap rejected")
                 
             del temp_placement
             
+            # Add to graph
             self.x.append(self.iteration)
             self.y.append(self.current_cost)
             
             
         debug_print("{}% accepted swaps".format(float(accepted_swap) / self.n_moves))
+        
+        # Update temperature
         self.temperature = self.temperature * temperature_rate
         if self.temperature < 0.1:
             self.temperature = 0
         debug_print("New temperature: {}".format(round(self.temperature, 2)))
         
+        # Update temperature label on GUI
         self.c.delete("temp")
         self.c.create_text(
             grid["right"] - 100,
@@ -397,17 +540,27 @@ class SimAnneal:
             
             
     def full_anneal(self):
+        '''
+        Run full simulated annealing
+        '''
+        
+        # Start with a random placement
         self.random_placement()
+        
+        # While the exit criteria has not been met, keep iterating
         while self.no_exit():
             self.anneal()
             
+        # Update the graph when annealing is complete
         self.animate()
-        cost = self.current_cost
-        print("Done! Cost = {}".format(cost))
+        
+        # Check the final cost
         self.calculate_cost()
-        # assert cost == self.current_cost
-        print("Cost = {}".format(self.current_cost))
+        print("Done! Cost = {}".format(self.current_cost))
+        
+        # Update final GUI
         self.draw_connections()
         self.update_labels()
         self.c.update()
+        
         return self.current_cost
--- gui.py
+++ gui.py
@@ -10,18 +10,24 @@ from settings import *
 from netlist_parser import *
 from annealing import *
 
+
+# Initialize the debug log
 debug_log.write("\n\n{}\n".format("="*20))
 debug_log.write(time.strftime("%Y-%m-%d %H:%M:%S\n", time.localtime()))
 debug_log.write("{}\n".format("="*20))
 
 
+# If only running 1 circuit
 if single_circuit:
+    
+    # Choose circuit
     filename = input("Name of circuit: ")
 
+    # Open circuit
     debug_print("Reading configurations for {}...".format(filename))
     configs, nets = parse_file("./circuits/{}.txt".format(filename))
 
-
+    # Initialize GUI
     root = Tk()
     grid["x"] = (grid["right"] - grid["left"]) / configs["cols"]
     grid["y"] = (grid["bottom"] - grid["top"]) / (configs["rows"] * 2 - 1)
@@ -46,12 +52,15 @@ if single_circuit:
         for y in range(configs["rows"]):
             c.create_line(grid["left"] + x * grid["x"], grid["top"] + (y * 2) * grid["y"], grid["left"] + x * grid["x"], grid["top"] + (y * 2 + 1) * grid["y"], fill=line_colour)
         
+    # Initialize graphing
     fig = Figure()
     ax = fig.add_subplot(111)
 
+    # Initialize annealing
     simulated_annealing = SimAnneal(c, ax)
     simulated_annealing.setup(configs, nets)
 
+    # Add buttons
     button_frame = Frame(root, width=screensize["width"])
     place_button = Button(button_frame, text ="Randomly Place", command=simulated_annealing.random_placement)
     it_button = Button(button_frame, text ="Iterate", command=simulated_annealing.anneal)
@@ -61,7 +70,8 @@ if single_circuit:
     place_button.grid(row=0, column=0)
     it_button.grid(row=0, column=1)
     anneal_button.grid(row=0, column=2)
-
+    
+    # Add graph
     graphFrame = Frame(root, width=screensize["width"])
     graphFrame.grid(row=0, column=1)
     graph = FigureCanvasTkAgg(fig, graphFrame)
@@ -69,34 +79,20 @@ if single_circuit:
     graph.get_tk_widget().pack(side=TOP, fill=BOTH, expand=1)
     ani = FuncAnimation(fig, simulated_annealing.animate, np.arange(1, 200), interval=graph_delay, blit=False)
 
-
     # Run GUI
     root.mainloop()
-    
+
+
+# Otherwise, run "benchmark"
 else:
-    results_log = open("logs/results__{}.txt".format(time.strftime("%Y-%m-%d_%H-%M-%S", time.localtime())), "w+")
+    
+    # Initialize results log
+    results_file = "results__{}.txt".format(time.strftime("%Y-%m-%d_%H-%M-%S", time.localtime()))
 
-    results_log.write("\n\n{}\n".format("="*20))
-    results_log.write(time.strftime("%Y-%m-%d %H:%M:%S\n", time.localtime()))
-    results_log.write("{}\n".format("="*20))
-
-    results_log.write("Settings\n")
-    results_log.write("Starting Temperature: {}\n".format(start_temperature))
-    results_log.write("Temperature Rate: {}\n".format(temperature_rate))
-    results_log.write("Exit Criteria: {}\n".format(exit_criteria))
-    results_log.write("Exit Temperature: {}\n".format(exit_temperature))
-    results_log.write("Exit Iterations: {}\n".format(exit_iterations))
-    results_log.write("Dynamic Moves Per Temperature: {}\n".format(dynamic_n_moves))
-    results_log.write("Moves Per Temperature: {}\n".format(n_moves))
-    results_log.write("k (Moves Per Temperature): {}\n".format(k_n_moves))
-    results_log.write("Range Window: {}\n".format(range_window))
-    results_log.write("Range Window Size: {}\n".format(window_size))
-    results_log.write("Shuffle: {}\n".format(shuffle))
-    results_log.write("Ripple: {}\n".format(ripple))
-    results_log.write("\n{}\n\n".format("*"*50))
-
+    # Get list of benchmark circuits
     circuits = [name for name in os.listdir("./circuits")]
     
+    # Initialize GUI
     root = Tk()
     frame = Frame(root, width=screensize["width"], height=screensize["height"])
     frame.grid(row=0, column=0)
@@ -106,12 +102,14 @@ else:
     fig = Figure()
     ax = fig.add_subplot(111)
     
+    # Initialize graph
     graphFrame = Frame(root, width=screensize["width"])
     graphFrame.grid(row=0, column=1)
     graph = FigureCanvasTkAgg(fig, graphFrame)
     graph.draw()
     graph.get_tk_widget().pack(side=TOP, fill=BOTH, expand=1)
 
+    # Initalize SA
     simulated_annealing = SimAnneal(c, ax)
 
     ani = FuncAnimation(fig, simulated_annealing.animate, np.arange(1, 200), interval=graph_delay, blit=False)
@@ -119,16 +117,21 @@ else:
     cumulative_cost = 0
     print("Running simulated annealing for {} benchmark circuits.".format(len(circuits)))
 
+    # Iterate through circuits
     for circuit in circuits:
         
+        # Load circuit
         print("Reading configurations for {}...".format(circuit))
         configs, nets = parse_file("./circuits/{}".format(circuit))
 
+        # Update GUI grid
         grid["x"] = (grid["right"] - grid["left"]) / configs["cols"]
         grid["y"] = (grid["bottom"] - grid["top"]) / (configs["rows"] * 2 - 1)
 
+        # Set up SA
         simulated_annealing.setup(configs, nets)
         
+        # Add circuit label to GUI
         c.create_text(
             grid["left"],
             20,
@@ -137,6 +140,8 @@ else:
             font=('Arial',20,'bold'),
             anchor=W
         )
+        
+        # Update GUI
         debug_print("Drawing grid...")
         for y in range(configs["rows"] * 2):
             c.create_line(grid["left"], grid["top"] + y * grid["y"], grid["right"], grid["top"] + y * grid["y"], fill=line_colour)
@@ -144,19 +149,28 @@ else:
             for y in range(configs["rows"]):
                 c.create_line(grid["left"] + x * grid["x"], grid["top"] + (y * 2) * grid["y"], grid["left"] + x * grid["x"], grid["top"] + (y * 2 + 1) * grid["y"], fill=line_colour)
 
+        # Run SA and track cost
         cost = simulated_annealing.full_anneal()
         cumulative_cost += cost
         
+        # Update cost in results log
+        results_log = open("logs/{}".format(results_file), "a")
         results_log.write("{circuit}\t{cost}\n".format(circuit=circuit.replace(".txt", ""), cost=cost))
+        results_log.close()
         
+        # Save an image of the output
         c.postscript(file="photos/{circuit}_{time}.ps".format(circuit=circuit.replace(".txt", ""), time=time.strftime("%Y-%m-%d_%H-%M", time.localtime())), colormode='color')
         
+        # Clear the canvas
         c.delete("all")
 
 
+    # Calculate average cost
     print("Final Average Cost: {}".format(float(cumulative_cost) / len(circuits)))
     results_log.write("\nFinal Average Cost: {}\n".format(float(cumulative_cost) / len(circuits)))
     ax.clear()
+    
+    # Update the GUI with final result
     c.create_text(
         screensize["width"] / 2,
         screensize["height"] / 2,
@@ -164,8 +178,8 @@ else:
         fill="black",
         font=('Arial',30,'bold')
     )
-    results_log.close()
     
+# Close debug log
 debug_log.close()
 
 # Run GUI
